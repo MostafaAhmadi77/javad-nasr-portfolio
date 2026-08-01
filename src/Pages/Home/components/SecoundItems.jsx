d490f5b54e363084d8feda32a229ccfb5b9d0272
MostafaAhmadi <mostafa.ahmadiii1377@gmail.com>
2026-08-01 23:03:09 +0330

    Change Name Project

--- src/Pages/Home/components/SecoundItems.jsx
+++ src/Pages/Home/components/SecoundItems.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
-export default function SecoundItems({title , des , icon:Icon}) {
+export default function SecoundItems({ title, des, icon: Icon }) {
   return (
-    <div className='flex justify-center items-center w-1xl gap-2'>
-<Icon className='text-amber-600 size-28 ' />
-<div>
-    <h2 className=' text-1xl font-bold text-white/85 font-serif'  >{title}</h2>
-    <p className='text-gray-200 text-sm '>{des}</p>
-</div>
+    <div className="flex justify-center items-center w-1xl gap-2 bg-[#11121187] rounded-2xl p-2">
+      <Icon className="text-amber-600 size-28 " />
+      <div>
+        <h2 className=" text-1xl font-bold text-white/85 font-serif">
+          {title}
+        </h2>
+        <p className="text-gray-200 text-sm ">{des}</p>
+      </div>
     </div>
-  )
+  );
 }
