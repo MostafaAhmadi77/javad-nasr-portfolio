97054b7abf0d9dd3311a39a79854cd38a2ac12bb
asalsharifzad <asalsharifzade@gmail.com>
2026-09-07 11:37:19 +0330

    add txtHyper in component Hero

--- src/Pages/About/Page.jsx
+++ src/Pages/About/Page.jsx
@@ -4,7 +4,8 @@ function About() {
   return (
     <>
       <div className="w-[4rem] h-[12rem] bg-amber-400">
-        <Hero titlePage="About"/>
+        <Hero titlePage="About" textHyper="HelloAmin"/>
+        
       </div>
     </>
   );
