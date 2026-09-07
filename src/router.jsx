4e25283686c12c713decee5ebca4947b3fd6d215
asalsharifzad <asalsharifzade@gmail.com>
2026-09-07 11:18:13 +0330

    ---

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,6 @@ import MainLayout from "./Components/Layouts/MainLayout";
 import Home from "./Pages/Home/Page";
 import NotFound from "./Components/404/NotFound";
 import Services from "./Pages/Services/Page";
-import About from "./Pages/About/Page"
 
 const router = createBrowserRouter([
     {
@@ -25,7 +24,7 @@ const router = createBrowserRouter([
             },
             {
                 path:"/about",
-                element:<About />
+                element:<NotFound />
             },
             {
                 path:"/process",
